d377713b2163d43a454aa86007adecc33e080b15
OLGON92 <ogon318@gmail.com>
2023-03-13 14:23:24 -0700

    add solution for prompt2

---
+++ prompt2.js
@@ -0,0 +1,9 @@
+function removeDuplicates(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (result.indexOf(arr[i]) === -1) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+}
